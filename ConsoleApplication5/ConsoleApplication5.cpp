57a6e0c80bb743363331abf72b00ea91c6a2075a
kamokar <kamokar@wp.pl>
2015-12-30 21:15:15 +0100

    Starting some fun

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+using namespace std;
+
+class zwierze {
+public:
+	virtual void glos() {
+		cout << "ZWIERZE daje glos" << endl;
+	}
+	zwierze() {
+		cout << "KONST zwierze" << endl;
+	};
+	~zwierze() {
+		cout << "DEST zwierze" << endl;
+	};
+};
+
+class pies : public zwierze {
+public:
+	void glos() {
+		cout << "PIES daje glos" << endl;
+	}	pies() {
+		cout << "KONST pies" << endl;
+	};
+	~pies() {
+		cout << "DEST pies" << endl;
+	};
+};
+
+class kot : public zwierze {
+public:
+	void glos() {
+		cout << "KOT daje glos" << endl;
+	}	kot() {
+		cout << "KONST kot" << endl;
+	};
+	~kot() {
+		cout << "DEST kot" << endl;
+	};
+};
+
+void glosik(zwierze *k) {
+	k->glos();
+};
+
+
+int main()
+{
+	
+		pies *pimpek = new pies;
+		kot *punia = new kot;
+
+		zwierze *zwierzak;
+
+		zwierzak = punia;
+		glosik(zwierzak);
+
+		zwierzak = pimpek;
+		glosik(zwierzak);
+
+		delete punia;
+		delete pimpek;
+		delete zwierzak;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	system("pause");
+	return(0);
+}
+
